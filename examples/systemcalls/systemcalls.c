18e8deb59aa48c8ea2dc83dbc08547351fd1e787
jedward1977 <john.esquiagola@gmail.com>
2024-11-02 21:18:25 -0500

    first task

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -1,4 +1,9 @@
 #include "systemcalls.h"
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+// #include <stdio.h>
 
 /**
  * @param cmd the command to execute with system()
@@ -9,6 +14,8 @@
 */
 bool do_system(const char *cmd)
 {
+    // printf("\n\n--------- do_system ------\n");
+    // printf("CMD: %s", cmd);
 
 /*
  * TODO  add your code here
@@ -16,8 +23,14 @@ bool do_system(const char *cmd)
  *   and return a boolean true if the system() call completed with success
  *   or false() if it returned a failure
 */
-
-    return true;
+	int ret;
+    ret =  system(cmd);
+	//printf("Status %d\n", ret);
+    if (ret == 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 /**
@@ -36,6 +49,7 @@ bool do_system(const char *cmd)
 
 bool do_exec(int count, ...)
 {
+    printf("\nExecuting do exec .......\n");
     va_list args;
     va_start(args, count);
     char * command[count+1];
@@ -43,6 +57,7 @@ bool do_exec(int count, ...)
     for(i=0; i<count; i++)
     {
         command[i] = va_arg(args, char *);
+        //printf("\nCmd : %s, i : %d\n",command[i], i);
     }
     command[count] = NULL;
     // this line is to avoid a compile warning before your implementation is complete
@@ -59,10 +74,54 @@ bool do_exec(int count, ...)
  *
 */
 
-    va_end(args);
+    int pid;
+    int status;
+    int wstatus;
+    int rstatus;
 
+    if ((pid = fork()) == -1)
+    {
+        perror("Error on fork:");
+        va_end(args);
+        return false;
+    }
+    else if (pid == 0) //child process
+    {
+        va_end(args);
+        if ((status = execv(command[0], command)) == -1)
+        {
+            perror("Error on execv:");
+            exit(1);
+        }
+        else
+            exit(0);
+    }
+    else //parent process
+    {
+        if ((pid = waitpid(-1, &wstatus, 0)) == -1)
+        {
+            perror("Error on waitpid:");
+            va_end(args);
+            return false;
+        }
+        else
+        {
+            if (WIFEXITED(wstatus))
+            {
+                if ((rstatus = WEXITSTATUS(wstatus)) == 1)
+                {
+                    printf("Error return from waitpid: %d\n", rstatus);
+                    return false;
+                }
+                else
+                    printf("Success return from waitpid: %d\n", rstatus);
+            }
+        }
+    }
+    va_end(args);
     return true;
 }
+
 
 /**
 * @param outputfile - The full path to the file to write with command output.
@@ -71,6 +130,7 @@ bool do_exec(int count, ...)
 */
 bool do_exec_redirect(const char *outputfile, int count, ...)
 {
+    printf("\n\n---> Executing do_exec_redirect ....\n");
     va_list args;
     va_start(args, count);
     char * command[count+1];
@@ -84,16 +144,79 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
     // and may be removed
     command[count] = command[count];
 
-
 /*
  * TODO
- *   Call execv, but first using https://stackoverflow.com/a/13784315/1446624 as a refernce,
+ *   Call execv, but first using https://stackoverflow.com/a/13784315/1446624 as a reference,
  *   redirect standard out to a file specified by outputfile.
  *   The rest of the behaviour is same as do_exec()
  *
 */
 
-    va_end(args);
+// printf(" AQUI----> cmd[0]: %s, cmd[1]: %s, cmd[2]: %s\n", command[0], command[1], command[2]);
 
+// First, create the file 
+    int fd = open(outputfile, O_WRONLY|O_TRUNC|O_CREAT, 0644);
+    if (fd < 0){
+        perror("open:");
+        va_end(args);
+        return false;
+    }
+
+    int pid;
+    int status;
+    int wstatus;
+    int rstatus;
+
+    if ((pid = fork()) == -1)
+     {
+         perror("Error on fork:");
+         va_end(args);
+         close(fd);
+         return false;
+     }
+     else if (pid == 0) //child process
+     {
+         va_end(args);
+         if (dup2(fd, 1) < 0)
+         {
+            perror("dup2");
+            close(fd);
+            exit(1);
+         }
+         close(fd);
+         if ((status = execv(command[0], command)) == -1)
+         {
+             perror("Error on execv:");
+             exit(1);
+         }
+         else
+             exit(0);
+     }
+     else //parent process
+     {
+         if ((pid = waitpid(-1, &wstatus, 0)) == -1)
+         {
+             perror("Error on waitpid:");
+             va_end(args);
+             close(fd);
+             return false;
+         }
+         else
+         {
+             if (WIFEXITED(wstatus))
+             {
+                 if ((rstatus = WEXITSTATUS(wstatus)) == 1)
+                 {
+                     printf("Error return from waitpid: %d\n", rstatus);
+                     close(fd);
+                     return false;
+                 }
+                 else
+                     printf("Success return from waitpid: %d\n", rstatus);
+             }
+         }
+     }
+    va_end(args);
+    close(fd);
     return true;
 }
